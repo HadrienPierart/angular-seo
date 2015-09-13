dc6389a4b52ebbd2b6dd93a2df8369a9653f0cfe
HadrienPierart <HadrienPierart@users.noreply.github.com>
2015-09-13 15:24:54 +0200

    Fix html5mode broken in library

--- angular-seo-server.js
+++ angular-seo-server.js
@@ -1,57 +1,64 @@
 var system = require('system');
 
 if (system.args.length < 3) {
-    console.log("Missing arguments.");
-    phantom.exit();
+  console.log("Missing arguments.");
+  phantom.exit();
 }
 
 var server = require('webserver').create();
 var port = parseInt(system.args[1]);
 var urlPrefix = system.args[2];
+var html5mode = system.args[3];
 
 var parse_qs = function(s) {
-    var queryString = {};
-    var a = document.createElement("a");
-    a.href = s;
-    a.search.replace(
-        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-        function($0, $1, $2, $3) { queryString[$1] = $3; }
-    );
-    return queryString;
+  var queryString = {};
+  var a = document.createElement("a");
+  a.href = s;
+  a.search.replace(
+      new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+      function($0, $1, $2, $3) { queryString[$1] = $3; }
+  );
+  return queryString;
 };
 
 var renderHtml = function(url, cb) {
-    var page = require('webpage').create();
-    page.settings.loadImages = false;
-    page.settings.localToRemoteUrlAccessEnabled = true;
-    page.onCallback = function() {
-        cb(page.content);
-        page.close();
-    };
+  var page = require('webpage').create();
+  page.settings.loadImages = false;
+  page.settings.localToRemoteUrlAccessEnabled = true;
+  page.onCallback = function() {
+    cb(page.content);
+    page.close();
+  };
 //    page.onConsoleMessage = function(msg, lineNum, sourceId) {
 //        console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
 //    };
-    page.onInitialized = function() {
-       page.evaluate(function() {
-            setTimeout(function() {
-                window.callPhantom();
-            }, 10000);
-        });
-    };
-    page.open(url);
+  page.onInitialized = function() {
+    page.evaluate(function() {
+      setTimeout(function() {
+        window.callPhantom();
+      }, 10000);
+    });
+  };
+  page.open(url);
 };
 
 server.listen(port, function (request, response) {
-    var route = parse_qs(request.url)._escaped_fragment_;
-    var url = urlPrefix
+
+  var routePrefix = html5mode ? '' : '#!';
+  var route = parse_qs(request.url)._escaped_fragment_;
+
+  if (!route || (route && route.indexOf('/') === -1)) route = '/' + route;
+
+  var url = urlPrefix
       + request.url.slice(1, request.url.indexOf('?'))
-      + '#!' + decodeURIComponent(route);
-    renderHtml(url, function(html) {
-        response.statusCode = 200;
-        response.write(html);
-        response.close();
-    });
+      + routePrefix + decodeURIComponent(route);
+  renderHtml(url, function(html) {
+    response.statusCode = 200;
+    response.write(html);
+    response.close();
+  });
 });
 
+console.log('html5mode is', (html5mode === 'true' ? 'activated' : 'not activated'));
 console.log('Listening on ' + port + '...');
 console.log('Press Ctrl+C to stop.');
